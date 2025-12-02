4e658b4c83989e2d0a5036af0f248491a45b991a
Leandro20001 <leandroferreira.dev@gmail.com>
2025-12-02 06:43:49 -0300

    atualizações

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function abrirDetalhe(jogo) {
     const texto = document.getElementById("detalheTexto");
 
     if (jogo === "hollow") {
-        banner.src = "https://cdn.cloudflare.steamstatic.com/steam/apps/367520/header.jpg";
-        preco.textContent = "R$46,99 🛒";
+        banner.src = "capa2.png";
+        preco.textContent = "R$46,99";
         imgs.innerHTML = `
-            <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/367520/ss_01.jpg">
-            <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/367520/ss_02.jpg">
-            <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/367520/ss_03.jpg">
+            <img src="scre1.webp">
+            <img src="scre2.png">
+            <img src="scre3.jpg">
         `;
         texto.textContent = "Hollow Knight é um jogo de ação e aventura em estilo metroidvania 2D, onde os jogadores exploram o vasto reino de Hallownest. É conhecido por seu combate de plataforma clássico com controles ajustados, um mundo extenso e interconectado, habilidades que evoluem e uma atmosfera sombria e rica em detalhes. O jogo apresenta uma grande variedade de inimigos e chefes épicos, além de personagens e cenários desenhados à mão, com uma trilha sonora atmosférica.";
     }
